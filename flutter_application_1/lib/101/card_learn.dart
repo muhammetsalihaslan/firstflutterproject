c20eb3b7bd3a4bcad3a56a5cdf0fedb972229912
Your Name <you@example.com>
2024-04-29 16:09:57 +0300

    Cardlearn

---
+++ flutter_application_1/lib/101/card_learn.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class CardLearn extends StatelessWidget {
+  const CardLearn({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: const Column(
+          children: [
+            _CustomCard(
+              child: SizedBox(
+                  height: 100, width: 500, child: Center(child: Text("Ali"))),
+            ),
+            Card(
+              margin: EdgeInsets.all(10),
+              child: SizedBox(height: 100, width: 100),
+            )
+          ],
+        ));
+  }
+}
+
+class _CustomCard extends StatelessWidget {
+  const _CustomCard({super.key, required this.child});
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    return const Card(
+        margin: EdgeInsets.all(10),
+        color: Colors.white,
+        shape: StadiumBorder(),
+        child: child);
+  }
+}
--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/101/padding_learn.dart';
+import 'package:flutter_application_1/101/card_learn.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
           appBarTheme: const AppBarTheme(
               centerTitle: true, backgroundColor: Colors.red, elevation: 0)),
-      home: const PaddingLearn(),
+      home: const CardLearn(),
     );
   }
 }
