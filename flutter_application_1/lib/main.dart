7ded766cf8550f16d13eae8b894924af4e4bbe02
Your Name <you@example.com>
2024-05-02 16:12:46 +0300

    colorlifecycle

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_application_1/101/text_field_learn.dart';
-import 'package:flutter_application_1/demos/color_demos_view.dart';
+import 'package:flutter_application_1/demos/color_life_cycle.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +20,7 @@ class MyApp extends StatelessWidget {
           progressIndicatorTheme:
               const ProgressIndicatorThemeData(color: Colors.white),
           appBarTheme: const AppBarTheme(centerTitle: true, elevation: 0)),
-      home: const ColorDemos(),
+      home: const ColorLifeCycle(),
     );
   }
 }
