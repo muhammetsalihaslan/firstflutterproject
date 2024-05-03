ae71d9a9e35834e064019ec0d0507c6b65b3a67c
Your Name <you@example.com>
2024-05-03 16:58:21 +0300

    model type

---
+++ flutter_application_1/lib/202/model_learn_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ModelLearnView extends StatefulWidget {
+  const ModelLearnView({super.key});
+
+  @override
+  State<ModelLearnView> createState() => _ModelLearnViewState();
+}
+
+class _ModelLearnViewState extends State<ModelLearnView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
