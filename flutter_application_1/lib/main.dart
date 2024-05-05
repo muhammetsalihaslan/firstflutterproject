57acb83586b58f11a1d76d741a5fe236e73b44dc
Your Name <you@example.com>
2024-05-05 16:16:12 +0300

    theme specification

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/202/package_learn_view.dart';
+import 'package:flutter_application_1/202/theme/light_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,12 +14,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Salih',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-          listTileTheme:
-              const ListTileThemeData(contentPadding: EdgeInsets.zero),
-          progressIndicatorTheme:
-              const ProgressIndicatorThemeData(color: Colors.white),
-          appBarTheme: const AppBarTheme(centerTitle: true, elevation: 0)),
+      theme: LightTheme().theme,
+      //  ThemeData.dark().copyWith(
+      //     listTileTheme:
+      //         const ListTileThemeData(contentPadding: EdgeInsets.zero),
+      //     progressIndicatorTheme:
+      //         const ProgressIndicatorThemeData(color: Colors.white),
+      //     appBarTheme: const AppBarTheme(centerTitle: true, elevation: 0)),
       home: const PackageLearn(),
     );
   }
